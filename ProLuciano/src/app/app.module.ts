3fa155305f7f8285301386a8b2a67569d03fd740
6luciano3 <99690369+6luciano3@users.noreply.github.com>
2022-08-10 20:23:54 -0300

    Componente

--- ProLuciano/src/app/app.module.ts
+++ ProLuciano/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeardComponent } from './components/heard/heard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeardComponent
   ],
   imports: [
     BrowserModule,
